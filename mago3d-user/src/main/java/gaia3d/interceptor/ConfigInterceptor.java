281546ca1e478ab76dc2f005159e15593f90e008
jdcheon <jdcheon@gaia3d.com>
2021-06-16 18:28:53 +0900

    메뉴 수정

--- mago3d-user/src/main/java/gaia3d/interceptor/ConfigInterceptor.java
+++ mago3d-user/src/main/java/gaia3d/interceptor/ConfigInterceptor.java
@@ -38,62 +38,64 @@ public class ConfigInterceptor implements HandlerInterceptor {
     	// TODO 너무 비 효율 적이다. 좋은 방법을 찾자.
     	// 세션이 존재하지 않는 경우
     	UserSession userSession = (UserSession)session.getAttribute(Key.USER_SESSION.name());
-    	if(userSession != null && userSession.getUserId() != null && !"".equals(userSession.getUserId())) {
-	    	List<UserGroupMenu> userGroupMenuList = CacheManager.getUserGroupMenuList(userSession.getUserGroupId());
-	    	Integer clickParentId = null;
-			Integer clickMenuId = null;
-			Integer clickDepth = null;
-			Menu menu = null;
-			Menu parentMenu = null;
-			
-			for(UserGroupMenu userGroupMenu : userGroupMenuList) {
-				if(uri.equals(userGroupMenu.getUrl())) {
-					clickMenuId = userGroupMenu.getMenuId();
-					if(userGroupMenu.getDepth() == 1) {
-						clickParentId = userGroupMenu.getMenuId();
-					} else {
-						clickParentId = Integer.valueOf(userGroupMenu.getParent().toString());
-					}
-					clickDepth = userGroupMenu.getDepth();
-					
-					if( userGroupMenu.getDepth() == 1 && (uri.indexOf("/main/index")>=0) ) {
-						break;
-					} else if( userGroupMenu.getDepth() == 2) {
-						break;
-					} else {
-						// pass
-					}
-				}
-			}
-			
-			menu = CacheManager.getMenuMap().get(clickMenuId);
-			parentMenu = CacheManager.getMenuMap().get(clickParentId);
-			if(menu != null) {
-				if(YOrN.Y == YOrN.valueOf(menu.getDisplayYn())) {
-					menu.setAliasName(null);
-					parentMenu.setAliasName(null);
+		String userId = userSession != null ? userId = userSession.getUserId() : null;
+		Integer userGroupId = userSession != null ? userGroupId = userSession.getUserGroupId() : 3;
+
+		List<UserGroupMenu> userGroupMenuList = CacheManager.getUserGroupMenuList(userGroupId);
+		Integer clickParentId = null;
+		Integer clickMenuId = null;
+		Integer clickDepth = null;
+		Menu menu = null;
+		Menu parentMenu = null;
+
+		for(UserGroupMenu userGroupMenu : userGroupMenuList) {
+			if(uri.equals(userGroupMenu.getUrl())) {
+				clickMenuId = userGroupMenu.getMenuId();
+				if(userGroupMenu.getDepth() == 1) {
+					clickParentId = userGroupMenu.getMenuId();
 				} else {
-					Integer aliasMenuId = CacheManager.getMenuUrlMap().get(menu.getUrlAlias());
-					Menu aliasMenu = CacheManager.getMenuMap().get(aliasMenuId);
-					menu.setAliasName(aliasMenu.getName());
-					parentMenu.setAliasName(aliasMenu.getName());
+					clickParentId = Integer.valueOf(userGroupMenu.getParent().toString());
+				}
+				clickDepth = userGroupMenu.getDepth();
+
+				if( userGroupMenu.getDepth() == 1 && (uri.indexOf("/main/index")>=0) ) {
+					break;
+				} else if( userGroupMenu.getDepth() == 2) {
+					break;
+				} else {
+					// pass
 				}
 			}
-			
-			
+		}
+
+		menu = CacheManager.getMenuMap().get(clickMenuId);
+		parentMenu = CacheManager.getMenuMap().get(clickParentId);
+		if(menu != null) {
+			if(YOrN.Y == YOrN.valueOf(menu.getDisplayYn())) {
+				menu.setAliasName(null);
+				parentMenu.setAliasName(null);
+			} else {
+				Integer aliasMenuId = CacheManager.getMenuUrlMap().get(menu.getUrlAlias());
+				Menu aliasMenu = CacheManager.getMenuMap().get(aliasMenuId);
+				menu.setAliasName(aliasMenu.getName());
+				parentMenu.setAliasName(aliasMenu.getName());
+			}
+		}
+
+
 //			Integer contentLoadBalancingIntervalValue = policy.getContent_load_balancing_interval().intValue() * 1000;
 //			request.setAttribute("contentLoadBalancingInterval", contentLoadBalancingIntervalValue);
-			request.setAttribute("profile", CacheManager.getProfile());
-			request.setAttribute("clickMenuId", clickMenuId);
+		request.setAttribute("profile", CacheManager.getProfile());
+		request.setAttribute("clickMenuId", clickMenuId);
 //			request.setAttribute("clickParentId", clickParentId);
 //			request.setAttribute("clickDepth", clickDepth);
-			request.setAttribute("menu", menu);
-			request.setAttribute("parentMenu", parentMenu);
-			
-			request.setAttribute("cacheUserGroupMenuList", userGroupMenuList);
-			request.setAttribute("cacheUserGroupMenuListSize", userGroupMenuList.size());
-			request.setAttribute("contentCacheVersion", CacheManager.getPolicy().getContentCacheVersion());
-    	}
+		request.setAttribute("menu", menu);
+		request.setAttribute("parentMenu", parentMenu);
+
+		request.setAttribute("cacheUserGroupMenuList", userGroupMenuList);
+		request.setAttribute("cacheUserGroupMenuListSize", userGroupMenuList.size());
+		request.setAttribute("contentCacheVersion", CacheManager.getPolicy().getContentCacheVersion());
+
     	
         return true;
     }
