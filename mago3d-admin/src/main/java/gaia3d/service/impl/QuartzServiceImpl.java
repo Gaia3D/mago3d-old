c9a3fe73b204f2d968d87d1a117e42160ab11dd1
jdcheon <jdcheon@gaia3d.com>
2021-07-05 14:28:09 +0900

    쿼츠 주석 처리

--- mago3d-admin/src/main/java/gaia3d/service/impl/QuartzServiceImpl.java
+++ mago3d-admin/src/main/java/gaia3d/service/impl/QuartzServiceImpl.java
@@ -1,25 +1,27 @@
-package gaia3d.service.impl;
-
-import gaia3d.service.QuartzService;
-import org.springframework.stereotype.Service;
-
-/**
- * 쿼츠
- * @author hansang
- *
- */
-@Service
-public class QuartzServiceImpl implements QuartzService {
-
-	/*@Autowired
-	private QuartzMapper quartzMapper;
-
-	*//**
-	 * SimpleTrigger 목록
-	 * @return
-	 *//*
-	public List<SimpleTrigger> getListSimpleTrigger(){;
-		return quartzMapper.getListSimpleTrigger();
-	}*/
-
-}
+//package gaia3d.service.impl;
+//
+//import gaia3d.persistence.QuartzMapper;
+//import gaia3d.service.QuartzService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 쿼츠
+// * @author hansang
+// *
+// */
+//@Service
+//public class QuartzServiceImpl implements QuartzService {
+//
+//	@Autowired
+//	private QuartzMapper quartzMapper;
+//
+//	/**
+//	 * SimpleTrigger 목록
+//	 * @return
+//	 */
+//	public List<SimpleTrigger> getListSimpleTrigger(){;
+//		return quartzMapper.getListSimpleTrigger();
+//	}
+//
+//}
