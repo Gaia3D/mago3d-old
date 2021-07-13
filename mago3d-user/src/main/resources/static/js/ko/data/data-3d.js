3dd73223d97f91f14d9ce5954568b45b0fb8819d
yhjeong1009 <yhjeong@gaia3d.com>
2021-07-13 22:53:57 +0900

    feat : 데이터 그룹 검색, 데이터 필터 템플릿 작업

--- mago3d-user/src/main/resources/static/js/ko/data/data-3d.js
+++ mago3d-user/src/main/resources/static/js/ko/data/data-3d.js
@@ -2,6 +2,13 @@ const Data3D = function(magoInstance) {
     this.magoInstance = magoInstance;
     this._load = false;
     this.datas = [];
+    this.dataGroups = [];
+
+    function setElementEvent() {
+
+    }
+    setElementEvent();
+
 }
 
 Object.defineProperties(Data3D.prototype, {
@@ -11,9 +18,7 @@ Object.defineProperties(Data3D.prototype, {
         },
         set : function (load) {
             if(load) {
-                //alert('1');
-                //this.initCamera();
-                //this.run();
+                this.getDataGroups(1);
             } else {
                 //this.clear();
             }
@@ -21,3 +26,59 @@ Object.defineProperties(Data3D.prototype, {
         }
     }
 });
+
+// 데이터 그룹 검색 ON/OFF
+Data3D.dataGroupSearchOnOff = function() {
+    $('#data-group-search').slideToggle();
+}
+
+// 데이터 검색 필터 ON/OFF
+Data3D.dataSearchFilterOnOff = function() {
+    $('#data-search-filter').slideToggle();
+}
+
+// 서버에서 데이터 그룹 정보 가져오기
+Data3D.prototype.getDataGroups = function(pageNo) {
+    const _this = this;
+
+    let params = _this.getFormData($("#search-data-group-form"));
+    params.pageNo = pageNo;
+    params.searchOption = "1";
+    const recursiveEncoded = $.param(params);
+    //{ pageNo : pageNo, searchWord : "data_group_name", searchValue : searchDataGroupName, searchOption : "1"};
+
+    $.ajax({
+        url: "/data-groups",
+        type: "GET",
+        headers: {"X-Requested-With": "XMLHttpRequest"},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: recursiveEncoded,
+        success: function(res){
+            if(res.statusCode <= 200) {
+                // html 생성
+                _this.dataGroups = res.dataGroupList;
+                //_this.createLayerTree();
+            } else {
+                alert(JS_MESSAGE[res.errorCode]);
+                console.log("---- " + res.message);
+            }
+        },
+        error: function(request, status, error) {
+            alert(JS_MESSAGE["ajax.error.message"]);
+        }
+    });
+}
+
+Data3D.prototype.getFormData = function($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+    $.map(unindexed_array, function (n, i) {
+        if (indexed_array[n['name']]) {
+            indexed_array[n['name']] += ',' + n['value'];
+        } else {
+            indexed_array[n['name']] = n['value'];
+        }
+    });
+    return indexed_array;
+}
